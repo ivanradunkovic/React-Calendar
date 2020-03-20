7a233252ecb48f2ff56e9d131ff9833d3cef1277
Ivan <ivan1501@gmail.com>
2020-03-20 17:50:34 +0100

    Bootstrap CSS added. Some small code cleanup and make it more readable.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App({ calendarStore }) {
     <div>
       <Router history={history}>
         <Navbar bg="primary" expand="lg" variant="dark">
-          <Navbar.Brand href="#home">Calendar App</Navbar.Brand>
+          <Navbar.Brand href="#home">Calendar</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
