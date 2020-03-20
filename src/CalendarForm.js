5da83c755b8572e27acf0dfae4c8d9e344b83b9c
Ivan <ivan1501@gmail.com>
2020-03-20 14:08:33 +0100

    Form completed. I can add and edit calendar now, added bootstrap.

--- src/CalendarForm.js
+++ src/CalendarForm.js
@@ -55,4 +55,74 @@ if (+start > +end) {
     calendarStore.setCalendarEvents(evs);
     onCancel();
 
-  export default observer(CalendarForm);
+};
+const handleStartChange = date => setStart(date);
+const handleEndChange = date => setEnd(date);
+const handleTitleChange = ev => setTitle(ev.target.value);
+const deleteCalendarEvent = async () => {
+  await deleteCalendar(calendarEvent.id);
+  const response = await getCalendar();
+  const evs = response.data.map(d => {
+    return {
+      ...d,
+      start: new Date(d.start),
+      end: new Date(d.end)
+    };
+  });
+  calendarStore.setCalendarEvents(evs);
+  onCancel();
+};
+
+return (
+    <Form noValidate onSubmit={handleSubmit}>
+      <Form.Row>
+        <Form.Group as={Col} md="12" controlId="title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={title || ""}
+            onChange={handleTitleChange}
+            isInvalid={!title}
+          />
+          <Form.Control.Feedback type="invalid">{!title}</Form.Control.Feedback>
+        </Form.Group>
+      </Form.Row>
+<Form.Row>
+        <Form.Group as={Col} md="12" controlId="start">
+          <Form.Label>Start</Form.Label>
+          <br />
+          <DatePicker
+            showTimeSelect
+            className="form-control"
+            selected={start}
+            onChange={handleStartChange}
+          />
+        </Form.Group>
+      </Form.Row>
+<Form.Row>
+        <Form.Group as={Col} md="12" controlId="end">
+          <Form.Label>End</Form.Label>
+          <br />
+          <DatePicker
+            showTimeSelect
+            className="form-control"
+            selected={end}
+            onChange={handleEndChange}
+          />
+        </Form.Group>
+      </Form.Row>
+      <Button type="submit" style={buttonStyle}>
+        Save
+      </Button>
+      <Button type="button" style={buttonStyle} onClick={deleteCalendarEvent}>
+        Delete
+      </Button>
+      <Button type="button" onClick={onCancel}>
+        Cancel
+      </Button>
+    </Form>
+  );
+}
+export default observer(CalendarForm);
