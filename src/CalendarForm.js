512b3f495356ac1c741492a7167870df116604a7
Ivan <ivan1501@gmail.com>
2020-03-20 13:59:53 +0100

    Calendar events added.

---
+++ src/CalendarForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import DatePicker from "react-datepicker";
+import Button from "react-bootstrap/Button";
+import {
+  addCalendar,
+  editCalendar,
+  getCalendar,
+  deleteCalendar } from "./requests";
+import { observer } from "mobx-react";
+
+const buttonStyle = { marginRight: 10 };
+function CalendarForm({ calendarStore, calendarEvent, onCancel, edit }) {
+  const [start, setStart] = React.useState(null);
+  const [end, setEnd] = React.useState(null);
+  const [title, setTitle] = React.useState("");
+  const [id, setId] = React.useState(null);
+
+  React.useEffect(() => {
+    setTitle(calendarEvent.title);
+    setStart(calendarEvent.start);
+    setEnd(calendarEvent.end);
+    setId(calendarEvent.id);
+  }, [
+    calendarEvent.title,
+    calendarEvent.start,
+    calendarEvent.end,
+    calendarEvent.id
+  ]);
+
+  export default observer(CalendarForm);
